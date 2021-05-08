18ec3533adc394b152974da7c10468a10d0e947b
JisuShin1 <82189683+JisuShin1@users.noreply.github.com>
2021-05-08 15:42:45 +0900

    Update books.c
    오류 수정하였습니다.

--- books.c
+++ books.c
@@ -3,37 +3,46 @@
 int createBooks(Book *b){
     printf("\n");
     printf("원하시는 책의 이름을 입력해주세요:");
-    scanf("%[^\n]s", b->name);
-    printf("저자의 이름을 입력해주세요:");
-    scanf("%[^\n]s", b->auth);
-    printf("책의 장르를 입력해주세요:");
-    scanf("%[^\n]s", b->genre);
-    printf("만약 대출이 되어있다면, 책의 대출일을 입력해주세요\n");
+    scanf("%s", b.name);
+    getchar();
+    printf("\n저자의 이름을 입력해주세요:");
+    scanf("%s", b.auth);
+    getchar();
+    printf("\n책의 장르를 입력해주세요:");
+    scanf("%s", b.genre);
+    getchar();
+    printf("\n만약 대출이 되어있다면, 책의 대출일을 입력해주세요\n");
     printf("대출이 되어있지 않다면 0000을 입력해주세요:");
-    scanf("%[^\n]s", b->borrDate);
-    printf("대출 여부를 입력해주세요:");
-    scanf("%d", b->borrAble);
+    scanf("%s", b.borrDate);
+    getchar();
+    printf("\n대출 여부를 입력해주세요(yes : 1, no : 0):");
+    scanf("%d", b.borrAble);
 }
 
 int updateBooks(Book *b){
     printf("\n");
     printf("수정하기를 원하시는 책의 이름을 입력해주세요:");
     scanf("%[^\n]s", b->name);
-    printf("수정하기를 원하시는 책의 저자의 이름을 입력해주세요:");
+    getchar();
+    printf("\n수정하기를 원하시는 책의 저자의 이름을 입력해주세요:");
     scanf("%[^\n]s", b->auth);
-    printf("수정하기를 원하시는 책의 장르를 입력해주세요:");
+    getchar();
+    printf("\n수정하기를 원하시는 책의 장르를 입력해주세요:");
     scanf("%[^\n]s", b->genre);
-    printf("수정하기를 원하시는 책이 만약 대출이 되어있다면, 책의 대출일을 입력해주세요\n");
+    getchar();
+    printf("\n수정하기를 원하시는 책이 만약 대출이 되어있다면, 책의 대출일을 입력해주세요\n");
     printf("대출이 되어있지 않다면 0000을 입력해주세요:");
     scanf("%[^\n]s", b->borrDate);
-    printf("수정하기를 원하시는 책의 대출 여부를 입력해주세요:");
+    getchar();
+    printf("\n수정하기를 원하시는 책의 대출 여부를 입력해주세요:");
     scanf("%d", b->borrAble);
+    getchar();
     printf("---------- (: 수정되었습니다 :) -----------");
 }
 
 int deleteBooks(Book *b){
     int yn =0;
-    printf("정말로 삭제하시겠습니까?(yes:1 no:0):");
+    printf("\n정말로 삭제하시겠습니까?(yes:1 no:0):");
     scanf("%d", &yn);
     if(yn == 1){
         b->borrAble = -1;

432afb5fabee8ea717cdc67f9f6906954d151354
cys01 <22000758@handong.edu>
2021-05-16 00:20:29 +0900

    Delete Useless while loop in update

--- books.c
+++ books.c
@@ -43,35 +43,32 @@ int updateBooks(Book *b, int index){
     readBooks(b, index);
     printf("수정하고 싶으신 책의 번호를 입력해주세요(취소:0)?");
     scanf("%d" , &no);
-    while(1){
-        if(no==0){
-		    printf("----취소되었습니다----");
-            continue;
-        }
-        printf("\n수정하기를 원하시는 책의 이름을 입력해주세요:");
-        scanf(" %[^\n]", b[no-1].name);
-        getchar();
-        printf("수정하기를 원하시는 책의 저자의 이름을 입력해주세요:");
-        scanf("%[^\n]", b[no-1].auth);
-        getchar();
-        printf("수정하기를 원하시는 책의 장르를 입력해주세요:");
-        scanf("%[^\n]", b[no-1].genre);
-        getchar();
-    	do {
-            printf("수정하기를 원하시는 책의 대출 가능 여부를 입력해주세요(yes : 1, no : 0):");
-            scanf("%d", &b[no-1].borrAble);
-	} while(b[no - 1].borrAble != 1 && b[no - 1].borrAble != 0);
-        if(b[no-1].borrAble)
-	    strcpy(b[no-1].borrDate, "00000000");
-        else {
-	        printf("수정하기를 원하시는 책의 대출일을 입력해주세요");
-	        scanf("%s", b[no-1].borrDate);
-	        getchar();
-        }
-        printf("---------- (: 수정되었습니다 :) -----------");
-        return 1;
+    if(no <= 0){
+	printf("----취소되었습니다----");
+        return 0;
     }
-	
+    printf("\n수정하기를 원하시는 책의 이름을 입력해주세요:");
+    scanf(" %[^\n]", b[no-1].name);
+    getchar();
+    printf("수정하기를 원하시는 책의 저자의 이름을 입력해주세요:");
+    scanf("%[^\n]", b[no-1].auth);
+    getchar();
+    printf("수정하기를 원하시는 책의 장르를 입력해주세요:");
+    scanf("%[^\n]", b[no-1].genre);
+    getchar();
+    do {
+        printf("수정하기를 원하시는 책의 대출 가능 여부를 입력해주세요(yes : 1, no : 0):");
+        scanf("%d", &b[no-1].borrAble);
+    }while(b[no - 1].borrAble != 1 && b[no - 1].borrAble != 0);
+    if(b[no-1].borrAble)
+	strcpy(b[no-1].borrDate, "00000000");
+    else {
+        printf("수정하기를 원하시는 책의 대출일을 입력해주세요");
+        scanf("%s", b[no-1].borrDate);
+        getchar();
+    }
+    printf("---------- (: 수정되었습니다 :) -----------");
+    return 1;
 }
 
 int deleteBooks(Book *b, int index){

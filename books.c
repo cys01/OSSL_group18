8fa09dd7e41b8c489595c90e4091e146811239da
cys01 <22000758@handong.edu>
2021-05-15 22:18:57 +0900

    Create Overdue book search function

--- books.c
+++ books.c
@@ -1,5 +1,7 @@
 #include "books.h"
 
+#define atoi(x) = (x - 48);
+
 int createBooks(Book *b){
     printf("\n원하시는 책의 이름을 입력해주세요:");
     scanf(" %[^\n]", b->name);
@@ -133,4 +135,56 @@ void searchBorrow(Book *b, int index) {
 		prinf("대출 가능한 도서를 찾을 수 없습니다...\n");
 }
 
-void searchOverdue(Book *b, int index);
+void searchOverdue(Book *b, int index) {
+	int isFound = 0;
+	int currDate = 0;
+	printf("오늘 날짜를 입력해주세요(예: 20210516) : ");
+	scanf("%d", &currDate);
+
+	for(int i = 0; i < index; i++) {
+		Book *book = b + i;
+		int isOver = 0;
+
+		int m2d = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+		char *date = book->borrDate;
+
+		int yyyy = atoi(date[0]) * 1000 + atoi(date[1]) * 100 + atoi(date[2]) * 10 + atoi(date[3]);
+		int mm = atoi(date[4]) * 10 + atoi(date[5]);
+		int dd = atoi(date[6]) * 10 + atoi(date[7]);
+
+		for(int i = 0; i < 6; i++) {
+			dd++;
+			if(dd > m2d[mm - 1]) {
+				dd = 1;
+				mm++;
+				if(mm == 13) {
+					mm++;
+					yyyy++;
+				}
+			}
+		}
+
+		if(currDate < yyyy * 10000 + mm * 100 + dd) {
+			printf("현재 날짜가 대출 날짜보다 이전에 있습니다!"); 
+			return;
+		}
+		else if((currDate / 1000) > yyyy)
+			isOver = 1;
+		else if((currDate / 100 % 100) > mm)
+			isOver = 1;
+		else if((currDate % 100) > dd)
+			isOver = 1;
+
+		if(!book->borrAble && isOver) {
+			if(!isFound) {
+				isFound = !isFound;
+				printf("%-12s\t%-12s\t%-8s\t%s\t\t%s\n", "이름", "저자", "장르", "대출일", "연체 여부");
+			}
+			printf("%-12s\t%-12s\t%-8s\t%-10s\t연체됨\n", book->name, book->auth, book->genre, book->borrDate);
+		}
+	}
+
+	if(!isFound)
+		prinf("연체된 도서를 찾을 수 없습니다...\n");
+}
